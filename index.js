04d78b2cdc35efa7d7d539fbe5bc4d429893f5f2
Beeno Tung <aabbcc1241@yahoo.com.hk>
2018-09-24 16:20:17 +0800

    tidy up code formatting; added global dep into devDep

--- index.js
+++ index.js
@@ -9,70 +9,72 @@ var tmpfilename = 'out.csv';
 
 function main() {
   return fetch(url)
-    .then(res=>res.text())
-    .then(text=>{
+    .then(res => res.text())
+    .then(text => {
       fs.writeFileSync(infilename, text);
-      xml2rec(infilename,'jtis_journey_time',tmpfilename);
+      xml2rec(infilename, 'jtis_journey_time', tmpfilename);
       // wait until the file is ready
-      return new Promise((resolve,reject)=>{
-        var loop = function(){
-          if(fs.existsSync(tmpfilename)){
+      return new Promise((resolve, reject) => {
+        var loop = function () {
+          if (fs.existsSync(tmpfilename)) {
             try {
-              if(fs.readFileSync(tmpfilename).toString()){
+              if (fs.readFileSync(tmpfilename).toString()) {
                 resolve();
                 return;
               }
             } catch (e) {
             }
           }
-          setTimeout(loop,100);
+          setTimeout(loop, 100);
         };
         loop();
       })
-        .then(()=>{
+        .then(() => {
           // the file is ready now
 
           // check if this is the first time
-          if (fs.existsSync(outfilename)){
+          if (fs.existsSync(outfilename)) {
             // not the first time, update existing file
             var text = fs.readFileSync(tmpfilename).toString();
             var lines = text.split('\n');
             lines.shift();
-            var last = lines[lines.length-1] || lines[lines.length-2];
+            var last = lines[lines.length - 1] || lines[lines.length - 2];
 
             // check if updated
             var _lines = fs.readFileSync(outfilename).toString().split('\n');
-            var _last = _lines[_lines.length-1] || _lines[_lines.length-2];
-            if (last != _last) {
+            var _last = _lines[_lines.length - 1] || _lines[_lines.length - 2];
+            if (last !== _last) {
               // remote data is updated, append to the local file
               text = lines.join('\n');
-              fs.appendFileSync(outfilename,text);
-              console.log('updated',outfilename);
+              fs.appendFileSync(outfilename, text);
+              console.log('updated', outfilename);
             } else {
               // remote data is not updated, done
               console.log('remote is not updated, skipping')
-            };
+            }
 
             fs.unlinkSync(tmpfilename);
           } else {
             // first time, just save to a new file
-            fs.renameSync(tmpfilename,outfilename);
-            console.log('saved to',outfilename);
-          };
+            fs.renameSync(tmpfilename, outfilename);
+            console.log('saved to', outfilename);
+          }
           fs.unlinkSync(infilename);
         });
     });
 }
-function run_main(){
-  console.log('checking for update:',new Date().toLocaleString());
+
+function run_main() {
+  console.log('checking for update:', new Date().toLocaleString());
   main()
-    .then(()=>{
+    .then(() => {
       console.log('waiting to check again after 2 minutes...');
-      setTimeout(run_main, 1000*60*2);
+      setTimeout(run_main, 1000 * 60 * 2);
     })
-    .catch(e=>{
+    .catch(e => {
       console.error(e);
       process.exit(1);
     });
 }
+
 run_main();
